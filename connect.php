6ea8d5a64931ca1926f22ebff33e1b2678e98c9a
Ambrose Chua <ambrose.chua.cm@gmail.com>
2016-10-31 01:30:37 +0800

    Change message.

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 $link = mysqli_connect(MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DATABASE);
 if(!$db) {
-	die("Unable to select database. ");
+	die("Unable to connect to database. ");
 }
 ?>
