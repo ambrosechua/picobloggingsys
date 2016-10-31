48967d0645203884cea9edbba55415b4dfe33b5b
Ambrose Chua <ambrose.chua.cm@gmail.com>
2016-10-31 12:47:48 +0800

    Fix multiple appearances when ids are off

--- get.php
+++ get.php
@@ -10,7 +10,7 @@ $result=mysqli_query($db, $qry);
 $newlastid=$_GET["lastid"];
 $jspo=array();
 while ($row = mysqli_fetch_array($result)) {
-$newlastid=$newlastid+1;
+$newlastid=$row["id"];
 array_push($jspo, array("txt"=>stripslashes($row["txt"]), "tim"=>$row["tim"], "id"=>$row["id"]));
 }
 echo json_encode(array("posts"=>$jspo, "lastid"=>$newlastid));
